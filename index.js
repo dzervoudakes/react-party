da2b711cd80e79008ebf3a873c60c6bcf98a426e
Dan Zervoudakes <6432641+dzervoudakes@users.noreply.github.com>
2018-03-22 10:11:00 -0600

    update port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const apiRoutes = require('./routes/api');
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.port || 8080;
 
 global.__dirname = __dirname;
 
