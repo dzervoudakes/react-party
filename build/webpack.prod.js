df87cef03368b64bd4bd7b9fd51c28fcfd386fef
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2018-03-27 18:23:27 -0600

    Update directory nomenclature

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -70,7 +70,7 @@ module.exports = merge(common, {
 		new HtmlWebpackPlugin({
 			favicon: `${PUBLIC_DIR}/favicon.ico`,
 			filename: 'index.html',
-			template: `${ROOT_DIR}/conf/templates/template.html`,
+			template: `${ROOT_DIR}/build/templates/template.html`,
 			title: 'Party!',
 			minify: {
 				removeComments: true,
