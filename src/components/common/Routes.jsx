0142c48fb286e3f67cdd4e4e103c02dd1c36e18e
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2018-04-03 22:00:10 -0600

    Tinkering

--- src/components/common/Routes.jsx
+++ src/components/common/Routes.jsx
@@ -2,23 +2,18 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Info, Faq, Rsvp } from '@/components';
 
-class Routes extends React.Component {
-	render() {
-		const { openDialog } = this.props;
-		return (
-			<Switch>
-				<Route exact path="/">
-					<Info openDialog={openDialog} />
-				</Route>
-				<Route path="/faq">
-					<Faq openDialog={openDialog} />
-				</Route>
-				<Route path="/rsvp">
-					<Rsvp openDialog={openDialog} />
-				</Route>
-			</Switch>
-		);
-	}
-}
+const Routes = ({ openDialog }) => (
+	<Switch>
+		<Route exact path="/">
+			<Info openDialog={openDialog} />
+		</Route>
+		<Route path="/faq">
+			<Faq openDialog={openDialog} />
+		</Route>
+		<Route path="/rsvp">
+			<Rsvp openDialog={openDialog} />
+		</Route>
+	</Switch>
+);
 
 export default Routes;
--- src/components/faq/FaqBlock.jsx
+++ src/components/faq/FaqBlock.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 
-class FaqBlock extends React.PureComponent {
-    render() {
-        const { answer, index, question, updatePanel } = this.props;
-        return (
-            <div className="faq-block" data-entry={index}>
-                <p className="question" onClick={() => updatePanel(index)}>{question}</p>
-                <p className="answer" dangerouslySetInnerHTML={{ __html: answer }} />
-            </div>
-        )
-    }
-}
+const FaqBlock = ({ answer, index, question, updatePanel }) => (
+	<div className="faq-block" data-entry={index}>
+		<p className="question" onClick={() => updatePanel(index)}>{question}</p>
+		<p className="answer" dangerouslySetInnerHTML={{ __html: answer }} />
+	</div>
+);
 
 export default FaqBlock;
