a9fd4a6b02a6d8c924c36a7c05537f3ff8ca8477
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2018-06-20 17:34:07 -0600

    Don't expose App.jsx from components/

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './components';
+import App from './components/App';
 import './sass/style';
 
 // Blue: #007bff
