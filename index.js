a24ca1b4c2853e878913711b0eabb1f387afa21d
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2018-06-23 10:25:25 -0600

    Incorporate webpack dev middleware

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fallback = require('connect-history-api-fallback');
 const apiRoutes = require('./routes/api');
 
 const app = express();
@@ -7,25 +8,36 @@ const port = process.env.port || 8080;
 
 global.__dirname = __dirname;
 
-app.get(/vendor\.(.*)\.min\.js/, (req, res, next) => {
-	req.url = `${req.url}.gz`;
-	res.set('Content-Encoding', 'gzip');
-	res.set('Content-Type', 'text/javascript');
-	next();
-});
+if (process.env.NODE_ENV === 'development') {
+	const webpack = require('webpack');
+	const webpackConfig = require('./build/webpack.local');
+	const webpackDev = require('webpack-dev-middleware');
 
-app.use(express.static(path.join(__dirname, '/public')));
+	const compiler = webpack(webpackConfig);
+	const devMiddleware = webpackDev(compiler, {
+		publicPath: '/',
+		quiet: true
+	});
 
-app.get(['/', '/faq', '/rsvp'], (req, res) => {
-	res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+	app.use(devMiddleware);
+}
 
+if (process.env.NODE_ENV === 'production') {
+	app.use((req, res) => {
+		res.sendFile(path.join(__dirname, '/public/index.html'));
+	});
+
+	app.get(/vendor\.(.*)\.min\.js/, (req, res, next) => {
+		req.url = `${req.url}.gz`;
+		res.set('Content-Encoding', 'gzip');
+		res.set('Content-Type', 'text/javascript');
+		next();
+	});
+}
+
+app.use(fallback());
 app.use('/api', apiRoutes);
-
-app.use((req, res) => {
-	res.status(404);
-	res.sendFile(path.join(__dirname, '/public/404.html'));
-});
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.listen(port, () => {
 	// eslint-disable-next-line
