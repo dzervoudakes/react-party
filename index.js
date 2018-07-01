417fe0d81308eb7b78eeb8b0b22fc8a44a516ca4
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2018-07-01 00:14:52 -0600

    Split out local server into its own file

--- index.js
+++ index.js
@@ -11,33 +11,8 @@ global.__dirname = __dirname;
 app.use(fallback());
 
 if (process.env.NODE_ENV === 'development') {
-	const webpack = require('webpack');
-	const webpackConfig = require('./build/webpack.dev');
-	const webpackDev = require('webpack-dev-middleware');
-	const webpackHot = require('webpack-hot-middleware');
-	const opn = require('opn');
-
-	const compiler = webpack(webpackConfig);
-	const devMiddleware = webpackDev(compiler, {
-		publicPath: '/',
-		quiet: true
-	});
-
-	const hotMiddleware = webpackHot(compiler, {
-		log: false,
-		heartbeat: 2000
-	});
-
-	compiler.plugin('compilation', compilation => {
-		compilation.plugin('html-webpack-plugin-after-emit', () => {
-			hotMiddleware.publish({ action: 'reload' });
-		});
-	});
-
-	devMiddleware.waitUntilValid(() => {
-		const url = `http://localhost:${port}`;
-		opn(url);
-	});
+	const server = require('./build/local-server')();
+	const { devMiddleware, hotMiddleware } = server;
 
 	app.use(devMiddleware);
 	app.use(hotMiddleware);
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -44,7 +44,7 @@ class App extends React.Component {
 					open={ open }
 					title={ title }
 				>
-					{message}
+					{ message }
 				</Dialog>
 				<Footer />
 			</div>
