a01dd407b3314fd7b5da4bda0917ba4cb63cbd58
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2018-07-02 21:04:13 -0600

    Tinker

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ global.__dirname = __dirname;
 app.use(fallback());
 
 if (process.env.NODE_ENV === 'development') {
-	const middleware = require('./build/dev-middleware')();
+	const middleware = require('./build/dev-middleware');
 	const { devMiddleware, hotMiddleware } = middleware;
 
 	app.use(devMiddleware);
