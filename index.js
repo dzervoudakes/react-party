80fd9f6b7d675c76a6446c2c857601ecee4918b3
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2018-09-25 20:46:20 -0600

    Update path joins

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ if (process.env.NODE_ENV === 'development') {
 
 if (process.env.NODE_ENV === 'production') {
 	app.get('/', (req, res) => {
-		res.sendFile(path.join(__dirname, '/public/index.html'));
+		res.sendFile(path.join(__dirname, 'public', 'index.html'));
 	});
 }
 
 app.use('/api', apiRoutes);
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
 	// eslint-disable-next-line
