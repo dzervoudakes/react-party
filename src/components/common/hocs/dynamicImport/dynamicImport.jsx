05a3c1bec7ce5eb189cb69f70d03b1434ac3535a
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2018-11-28 19:24:12 -0700

    Gettin' OCDish

--- src/components/common/hocs/dynamicImport/dynamicImport.jsx
+++ src/components/common/hocs/dynamicImport/dynamicImport.jsx
@@ -1,12 +1,12 @@
 import React, { Suspense } from 'react';
 // import { Something } from 'material-ui/something';
 
-const dynamicImport = ImportComponent => {
-	return props => (
+const dynamicImport = ImportComponent => (
+	props => (
 		<Suspense fallback={ <div>placeholder for something else</div> }>
 			<ImportComponent { ...props } />
 		</Suspense>
-	);
-};
+	)
+);
 
 export default dynamicImport;
